298a6dc49f45149b45e3e8c670765781e55ec712
IeuanBissell <150674465+IeuanBissell@users.noreply.github.com>
2026-03-17 14:41:45 +0000

    Created missing.py to begin imputation and removal of columns.

---
+++ pipeline/preprocessors/missing.py
@@ -0,0 +1,9 @@
+class MissingValueHandler:
+
+    def __init__(self, drop_col, mean_col, median_col, zero_cols):
+        self.drop_col = drop_col
+        self.mean_col = mean_col
+        self.median_col = median_col
+        self.zero_cols = zero_cols
+
+
